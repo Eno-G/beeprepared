e087ede7279f26d47261ebe0a9d593b2891eda2b
Eno-G <67418510+Eno-G@users.noreply.github.com>
2021-10-06 13:00:03 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'database_name_here' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'beeprepared@123' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
